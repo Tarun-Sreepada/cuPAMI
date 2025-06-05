ebe69ba783a156e89c4217e66c55b142a99bc1a0
Tarun Sreepada <tarunsreepada@tutanota.com>
2025-06-05 14:18:09 +0900

    more stuff

--- algs/frequent/frequent.py
+++
@@ -1,9 +0,0 @@
-from algs.base_algorithm import BaseAlgorithm
-
-class Frequent(BaseAlgorithm):
-    def mine(self):
-        """Run the algorithm."""
-        data_path = 'datasets/frequent/frequent.txt'
-        data = self.readFile(data_path)
-        # TODO: implement algorithm logic using `data`
-        pass
